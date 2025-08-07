a908ac61876be2bc5166716a70539131b0b9ba2f
Sophie <yummylemon8@gmail.com>
2025-08-07 10:49:51 -0700

    added second view added second page + navigation links/title

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -11,17 +11,28 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack{
-               Text("🫚")
-            Text("This is the root view!")
+                Text("🫚")
+                Text("This is the root view!")
+                    .font(.title)
+                
                 NavigationLink(destination: Text("You've arrived at the second page!")) {
                     Text("Continue...")
-                                    }
+                }
+                NavigationLink(destination: SecondView())
+                {
+                Text("What else is there?")
+                }
+                
+                }
+                               
+            .navigationTitle("to the roots")                               
+                               
             }//VStack
-            }//Nav Stack
-            
-    }
-}
-
+        }//Nav Stack
+        
+        
+    
+} // struct
 #Preview {
     ContentView()
 }
